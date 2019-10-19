6143857b83201076fca78f5f8449619a24c9e5a3
Shaun T Martin <webmaster@cototal.com>
2019-10-19 10:40:04 -0500

    Update build scripts to use docker hub.

--- build-dev.sh
+++ build-dev.sh
@@ -1,5 +1,4 @@
 export REV=`git rev-parse HEAD`
-export IMG_NAME=$GLR/docker-php-apache:${REV:0:7}-dev
+export IMG_NAME=cototal/php-apache:${REV:0:7}-dev
 docker build --build-arg PHP_ENV=development -t $IMG_NAME .
 docker push $IMG_NAME
-
