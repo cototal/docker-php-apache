31fbccf3ba1ca65dfb2a83152cb03fffe4f688a2
Shaun Martin <webmaster@cototal.com>
2020-02-02 19:42:05 -0600

    Make build scripts executable.

--- build-dev.sh
+++ build-dev.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export REV=`git rev-parse HEAD`
 export IMG_NAME=cototal/php-apache:${REV:0:7}-dev
 docker build --build-arg PHP_ENV=development -t $IMG_NAME .
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export REV=`git rev-parse HEAD`
 export IMG_NAME=cototal/php-apache:${REV:0:7}
 docker build -t $IMG_NAME .
